ab5f48def7f5a07e8499a2cba2adcd828292bf9e
amr-raazi <50888029+amr-raazi@users.noreply.github.com>
2020-09-12 13:56:17 +0530

    Minor changes

--- word frequency.py
+++ word frequency.py
@@ -1,9 +1,9 @@
-import pandas
+import pandas as pd
 import os
 
 # user inputted variables
 folder_path = r"excel_files"
-data_row_number = 1
+data_row_number = 3
 
 # program variable
 file_list = os.listdir(folder_path)
@@ -12,14 +12,13 @@ file_list = os.listdir(folder_path)
 os.chdir(folder_path)
 
 # create empty dataframe
-master_df = pandas.DataFrame()
+master_df = pd.DataFrame()
 
 # go through files and append data into master dataframe
 for file in file_list:
-    if file.endswith('.xlsx'):
-        master_df = master_df.append(pandas.read_excel(file, header=None, skiprows=1), ignore_index=True, )
+    master_df = master_df.append(pd.read_excel(file, header=None, skiprows=25), ignore_index=True, )
 
-# set new header for the row whose word frequency is to be printed
+# set new header for the row whose word frequency is to be printed in the master dataframe
 try:
     new_header = list(master_df.columns.values)
     new_header[data_row_number] = "data"
@@ -28,8 +27,11 @@ except IndexError:
     print("Row specified is not in the given columns or no file given")
     exit()
 
+# define new dataframe for frequency data
+frequency_data = master_df["data"].dropna().to_frame()
+
 # get frequency data from master dataframe
-frequency = master_df.data.str.split(expand=True).stack().value_counts()
+frequency = frequency_data.data.str.split(expand=True).stack().value_counts().to_frame()
 
 # save to excel
-pandas.frequency.to_excel("word frequency.xlsx")
+frequency.to_excel("word frequency.xlsx")
